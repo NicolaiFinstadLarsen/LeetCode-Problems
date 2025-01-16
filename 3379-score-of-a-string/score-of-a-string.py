8c5409a567c5cbd0aeeb607c121880d2f85404b9
Nicolai Finstad Larsen <59092220+NicolaiFinstadLarsen@users.noreply.github.com>
2025-01-16 06:33:05 +0100

    Time: 0 ms (100.00%) | Memory: 17.8 MB (16.30%) - LeetSync

--- 3379-score-of-a-string/score-of-a-string.py
+++ 3379-score-of-a-string/score-of-a-string.py
@@ -1,11 +1,21 @@
 class Solution:
     def scoreOfString(self, s: str) -> int:
-        my_list = []
-        for c in s:
+        #my_list = []
+        #for c in s:
             #print(ord(c))
-            my_list.append(ord(c))
-        new_list = []
-        for i in range(len(my_list) - 1):
-            new_list.append(abs(my_list[i] - my_list[i+1]))
-        return(sum(new_list))
+            #my_list.append(ord(c))
+        #new_list = []
+        #for i in range(len(my_list) - 1):
+            #new_list.append(abs(my_list[i] - my_list[i+1]))
+        #return(sum(new_list))
+
+        # Enklere kode
+        part_sum = 0
+        total = 0
+        for c in range(len(s) - 1):
+            part_sum = abs(ord(s[c]) - ord(s[c+1]))
+            total += part_sum
+            #print(part_sum)
+            #print(total)
+        return total
         
